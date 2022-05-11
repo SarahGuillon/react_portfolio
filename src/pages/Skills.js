ffe074950f6aa10c18f2d2a34ed78406a55781f5
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-05-11 23:36:03 +0200

    start of skills page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,8 +1,47 @@
+import '../styles/Skills.css';
+
 const Skills = () => {
   return (
     <div className="card">
       <div className="title">
         <h1> Skills</h1>
+      </div>
+      <div className="skills-details">
+        <h2>Web</h2>
+        <div class="chart">
+          <span>Ruby on Rails</span>
+          <footer>
+            <div className='fulfill'>60%</div>
+            <div className='empty'>40%</div>
+          </footer>
+        </div>
+          <div class="chart">
+          <span>CSS</span>
+          <footer>
+            <div className='fulfill'>60%</div>
+            <div className='empty'>40%</div>
+          </footer>
+        </div>
+          <div class="chart">
+          <span>Html</span>
+          <footer>
+            <div className='fulfill'>60%</div>
+            <div className='empty'>40%</div>
+          </footer>
+        </div>
+        {/* <p>Ruby on Rails</p>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JS</p>
+      </div>
+      <div>
+        <h2>Data</h2>
+      </div>
+      <div>
+        <h2>Project Management</h2>
+      </div>
+      <div>
+  <h2>Soft skills</h2>*/}
       </div>
     </div>
   );
