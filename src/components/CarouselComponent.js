3ef31ec9d64adc11e11c8c3bc8686185df39ff9a
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-05-30 18:54:50 +0200

    project page

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -1,47 +1,55 @@
 import React from "react";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import BetNews1 from '../images/BetNews1.png';
-import BetNews2 from '../images/BetNews2.png';
-import BetNews3 from '../images/BetNews3.png';
-import BetNews4 from '../images/BetNews4.png';
-import BetNews5 from '../images/BetNews5.png';
-import BetNews6 from '../images/BetNews6.png';
+
 import {MarkGithubIcon} from '@primer/octicons-react';
 import { faMobileScreen, faCode} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const CarouselComponent = () =>  {
+const CarouselComponent = (props) =>  {
+
+  const projectName = props.projectName;
+  const projectPicture = props.projectPicture;
+  const projectText = props.projectText;
+  const projectFramework = props.projectFramework;
+  const projectWebLink = props.projectWebLink;
+  const projectGithubLink = props.projectGithubLink;
+
+
+
     return (
       <div className="carousel-wrapper">
-        <Carousel infiniteLoop useKeyboardArrows>
+        <div className="project-title">
+          <h2>{projectName} project</h2>
+        </div>
+        <div className="project-slide">
+          <Carousel infiniteLoop useKeyboardArrows>
             <div>
-              <img src={BetNews1} alt="BetNews image" className="project-image" />
+              <img src={projectPicture[0]} alt="project screenshot" className="project-picture" />
             </div>
             <div>
-              <img src={BetNews2} alt="BetNews image" className="project-image" />
+              <img src={projectPicture[1]} className="project-picture" />
             </div>
             <div>
-              <img src={BetNews3} alt="BetNews image" className="project-image" />
+              <img src={projectPicture[2]} className="project-picture" />
             </div>
             <div>
-              <img src={BetNews4} alt="BetNews image" className="project-image" />
+              <img src={projectPicture[3]} className="project-picture" />
             </div>
             <div>
-              <img src={BetNews5} alt="BetNews image" className="project-image" />
+              <img src={projectPicture[4]} className="project-picture" />
             </div>
             <div>
-              <img src={BetNews6} alt="BetNews image" className="project-image" />
+              <img src={projectPicture[5]} className="project-picture" />
             </div>
-        </Carousel>
-        <div className="project-description">
-          <h2> BetNews </h2>
-          <p> BetNews is an mobile application created in 2 weeks with 3 other students of Le Wagon, Nantes. This is the final project after 9 weeks of an intensive bootcamp. <br></br>BetsNews targets people between 18 and 30 years old who are not really interested in traditional media but want to follow the news while having fun. With BetNews, you can bet on the news with betcoins (after studying the odds), you can create your own bet and you can challenge your friends to the leaderboard. The bets are classified by categories (Economy, Politic, Planet, Entertainment, Culture, Sport...). If you want to learn more about a subject before betting, you can read more information and find additionnal links to medias by clicking on the "bet card".</p>
-          <br />
-          <div className="icons">
-            <p><FontAwesomeIcon icon={faCode} /> <strong> Ruby on Rails</strong>  </p>
-            <p><MarkGithubIcon size={16} /><a href="https://github.com/Xastiro/BetNews" target="_blank" rel="noopener noreferrer">   https://github.com/Xastiro/BetNews</a></p>
-            <p><FontAwesomeIcon icon={faMobileScreen} /> <a href="http://www.betnews.fr/" target="_blank" rel="noopener noreferrer">   http://www.betnews.fr/</a></p>
+          </Carousel>
+          <div className="project-text">
+            {projectText}
+            <div className="icons">
+              <p><FontAwesomeIcon icon={faCode} /> <span> {projectFramework}</span>  </p>
+              <p><MarkGithubIcon size={16} /><a href={projectGithubLink} target="_blank" rel="noopener noreferrer">   {projectGithubLink}</a></p>
+              <p><FontAwesomeIcon icon={faMobileScreen} /> <a href={projectWebLink} target="_blank" rel="noopener noreferrer">  {projectWebLink}</a></p>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,33 +2,73 @@ import ProjectCard from '../components/ProjectCard';
 import ProjectCardHover from '../components/ProjectCardHover';
 import '../styles/Projects.css';
 import { useState } from 'react';
-// import CarouselComponent from '../components/CarouselComponent';
+import CarouselComponent from '../components/CarouselComponent';
+import BetNews1 from '../images/BetNews1.png';
+import BetNews2 from '../images/BetNews2.png';
+import BetNews3 from '../images/BetNews3.png';
+import BetNews4 from '../images/BetNews4.png';
+import BetNews5 from '../images/BetNews5.png';
+import BetNews6 from '../images/BetNews6.png';
+import baobab1 from '../images/baobab1.png';
+import baobab2 from '../images/baobab2.png';
+import baobab3 from '../images/baobab3.png';
+import baobab4 from '../images/baobab4.png';
+import baobab5 from '../images/baobab5.png';
+import baobab6 from '../images/baobab6.png';
 
 
 
 const Projects = () => {
 
+  const [betNewsSelected, setbetNewsSelected] = useState(false);
+  const [BAOnBSelected, setBAOnBSelected] = useState(false);
+  const [portfolioSelected, setPortfolioSelected] = useState(false);
   const [projectHoverOne, setProjectHoverOne] = useState(false);
   const [projectHoverTwo, setProjectHoverTwo] = useState(false);
   const [projectHoverTree, setProjectHoverTree] = useState(false);
+  const betNewsPictures = [BetNews1, BetNews2, BetNews3, BetNews4, BetNews5, BetNews6];
+  const BAOnBPictures = [baobab1, baobab2, baobab3, baobab4, baobab5, baobab6];
+
+  const betNewsProjectText = () => {
+    return (
+    <div>
+      <p> BetNews is an mobile application created in 2 weeks with 3 other students of Le Wagon. This is the final project after 9 weeks of an intensive bootcamp.<br></br><br></br>BetNews targets people between 18 and 30 years old who are not really interested in traditional media but want to follow the news while having fun. <br></br><br></br>With BetNews, you can bet on the news with betcoins (after studying the odds), you can create your own bet and you can challenge your friends to the leaderboard. Before betting, you can read articles to learn more about the subject selected. The bets are classified by categories (Economy, Politic, Planet, Entertainment, Culture, Sport...).</p>
+    </div>)
+  }
+
+  const BAOnBProjectText = () => {
+    return (
+    <div>
+      <p> BAO-n-B is a website made in 1 week by a team of 4 students of Le Wagon, Nantes. <br/><br/>The goal was to create our first marketplace. This web app was generated with lewagon/rails-templates with the framework Ruby on Rails. With BAOnB, you can book or rent a baboab or rent during the period you want ! To create this app, we used Ruby, HTML, CSS Simple form, bootstrap, Cloudinary, Heroku and the mapbox API.</p>
+    </div>)
+  }
+
+  const handleClose = () => {
+    setbetNewsSelected(false);
+    setBAOnBSelected(false);
+    setPortfolioSelected(false);
+  }
 
   return (
     <div className="projects-content">
+      { (betNewsSelected || BAOnBSelected || portfolioSelected) && <p className="close" onClick={handleClose}> X </p>}
       <div className="title">
-        <h1>Projects</h1>
+        {!betNewsSelected && !BAOnBSelected && !portfolioSelected && <h1>Projects</h1>}
       </div>
       <div className="projects-container" >
-        <div onMouseEnter={(e)=> setProjectHoverOne(true)} onMouseLeave={(e)=> setProjectHoverOne(false)}>
-          {!projectHoverOne && <ProjectCard projectName={"BetNews"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(255, 208, 132, 0.15)), url(https://images.unsplash.com/photo-1611159063981-b8c8c4301869)'}/>}
-          { projectHoverOne && <ProjectCardHover projectName={"view project"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(255, 208, 132, 0.15)), url(https://images.unsplash.com/photo-1611159063981-b8c8c4301869)'}/>}
+        <div onMouseEnter={(e)=> setProjectHoverOne(true)} onMouseLeave={(e)=> setProjectHoverOne(false)} onClick={(e) => setbetNewsSelected(true)}>
+          {!betNewsSelected && !BAOnBSelected && !portfolioSelected && !projectHoverOne && <ProjectCard projectName={"BetNews"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(255, 208, 132, 0.15)), url(https://images.unsplash.com/photo-1611159063981-b8c8c4301869)'}/>}
+          {!betNewsSelected && !BAOnBSelected && !portfolioSelected && projectHoverOne && <ProjectCardHover projectName={"view project"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(255, 208, 132, 0.15)), url(https://images.unsplash.com/photo-1611159063981-b8c8c4301869)'}/>}
+          {betNewsSelected && <CarouselComponent projectPicture={betNewsPictures} projectName={"BetNews"} projectText={betNewsProjectText()} projectFramework={"Ruby on Rails"} projectGithubLink={"https://github.com/Xastiro/BetNews"} projectWebLink={"http://www.betnews.fr/"}/>}
         </div>
-        <div onMouseEnter={(e)=> setProjectHoverTwo(true)} onMouseLeave={(e)=> setProjectHoverTwo(false)}>
-          {!projectHoverTwo && <ProjectCard projectName={"Bao'n'B"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(0,0,0,0.2)), url(https://images.unsplash.com/photo-1570742544137-3a469196c32b?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869)'}/>}
-          { projectHoverTwo && <ProjectCardHover projectName={"view project"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(0,0,0,0.2)), url(https://images.unsplash.com/photo-1570742544137-3a469196c32b?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869)'}/>}
+        <div onMouseEnter={(e)=> setProjectHoverTwo(true)} onMouseLeave={(e)=> setProjectHoverTwo(false)} onClick={(e) => setBAOnBSelected(true)}>
+          {!betNewsSelected && !BAOnBSelected && !portfolioSelected && !projectHoverTwo && <ProjectCard projectName={"Bao'n'B"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(0,0,0,0.2)), url(https://images.unsplash.com/photo-1570742544137-3a469196c32b?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869)'}/>}
+          {!betNewsSelected && !BAOnBSelected && !portfolioSelected && projectHoverTwo && <ProjectCardHover projectName={"view project"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(0,0,0,0.2)), url(https://images.unsplash.com/photo-1570742544137-3a469196c32b?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869)'}/>}
+          {BAOnBSelected && <CarouselComponent projectPicture={BAOnBPictures} projectName={"BAOnB"} projectText={BAOnBProjectText()} projectFramework={"Ruby on Rails"} projectGithubLink={"https://github.com/Hugo-Ferrand/baobab"} projectWebLink={"https://stormy-brushlands-99936.herokuapp.com/"}/>}
         </div>
         <div onMouseEnter={(e)=> setProjectHoverTree(true)} onMouseLeave={(e)=> setProjectHoverTree(false)}>
-          {!projectHoverTree && <ProjectCard projectName={"Portfolio"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(0,0,0,0.2)), url(https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170)'}/>}
-          { projectHoverTree && <ProjectCardHover projectName={"view project"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(0,0,0,0.2)), url(https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170)'}/>}
+          {!betNewsSelected && !BAOnBSelected && !portfolioSelected && !projectHoverTree && <ProjectCard projectName={"Portfolio"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(0,0,0,0.2)), url(https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170)'}/>}
+          {!betNewsSelected && !BAOnBSelected && !portfolioSelected && projectHoverTree && <ProjectCardHover projectName={"view project"} projectPicture={'linear-gradient(rgba(255, 208, 132, 0.15), rgba(0,0,0,0.2)), url(https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170)'}/>}
         </div>
       </div>
     </div>
